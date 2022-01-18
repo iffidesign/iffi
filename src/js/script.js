ede642c74db84e73cf178816fbd3212250f70c09
Ang Zi Jie <purporz@gmail.com>
2022-01-18 09:57:13 +0800

    Set up parcel

---
+++ src/js/script.js
@@ -0,0 +1,51 @@
+window.$ = require('jquery');
+// import 'slick-carousel/slick/slick';
+
+{
+  $('.js--open-menu').on('click', function(){
+    $('.navigation__menu').addClass('active');
+    $('body').addClass('disable-scroll');
+  });
+  $('.js--close-menu').on('click', function(){
+    $('.navigation__menu').removeClass('active');
+    $('body').removeClass('disable-scroll');
+  });
+
+  function openPopup(target) {
+    let $target = $(target);
+    $target.addClass('show');
+    $('body').addClass('show-popup');
+
+    $target.find('.js--slick-slider').slick({
+      centerMode: true,
+      centerPadding: '90px',
+      slidesToShow: 1,
+      arrows: true,
+      prevArrow: '<span class="carousel-control-prev-icon slick__button slick--prev" aria-hidden="true"></span>',
+      nextArrow: '<span class="carousel-control-next-icon slick__button slick--next" aria-hidden="true"></span>',
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            centerMode: true,
+            centerPadding: '30px',
+            arrows: false
+          }
+        }
+      ]
+    });
+  }
+
+  $('[data-toggle="popup"]').on('click', function(){
+    let target = $(this).attr('data-target');
+    openPopup(target)
+  });
+
+  $('.js--close-popup').on('click', function(){
+    let $popup = $(this).closest('.popup')
+
+    $('body').removeClass('show-popup');
+    $popup.removeClass('show');
+    $popup.find('.js--slick-slider').slick('unslick');
+  });
+}
